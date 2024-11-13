e71c03a704bd0c1cb4f67723f5323e539f78e83d
Juan Ludevid <jmludevid@hotmail.com>
2024-11-13 21:37:19 +1100

    update state

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 
+// parse incoming requests with JSON payloads and make them available as req.body
 app.use(bodyParser.json());
+
+// server to listen on port 4000
 const port = 4000;
 
 const posts = {};
@@ -15,13 +18,16 @@ app.use(cors());
 app.get("/posts", async (req, res) => {
   res.send(posts);
 });
+
 app.post("/posts", (req, res) => {
   const id = randomBytes(4).toString("hex");
-  console.log(req);
+  console.log(req.body.title);
   const { title } = req.body;
   posts[id] = { id, title };
+  // new resource has been successfully created on the server
   res.status(201).send(posts[id]);
 });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
